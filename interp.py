27b4e63ab1da593a535c3cf8ef558b8d99c9e744
Caio Ariede <caio.ariede@gmail.com>
2016-01-01 12:01:26 -0200

    functions working partially

--- interp.py
+++ interp.py
@@ -1,4 +1,5 @@
 import ast
+import astor
 
 from collections import namedtuple
 
@@ -21,6 +22,8 @@ def parse(txt):
         for k, v in __builtins__.__dict__.items()
     }
 
+    scope['__'] = []
+
     buf = None
 
     for c in txt:
@@ -33,6 +36,11 @@ def parse(txt):
                     buf = None
                 else:
                     buf = (buft, bufv + c)
+            elif c == ',' and buft not in (list,):
+                push_stack(stack, buf)
+                push_stack(stack, (None, c))
+                yield from run_stack(stack, scope)
+                buf = None
             elif c == ' ':
                 push_stack(stack, buf)
                 buf = None
@@ -58,7 +66,12 @@ def parse(txt):
             else:
                 buf = (buft, bufv + c)
 
-        elif c == ' ':
+        elif c == ',':
+            push_stack(stack, (None, c))
+            yield from run_stack(stack, scope)
+        elif c == '.':
+            yield from run_stack(stack, scope)
+        elif c in (' ', '\r', '\n'):
             pass
         elif c == '"':
             buf = (str, "")
@@ -77,7 +90,7 @@ def parse(txt):
     yield from run_stack(stack, scope)
 
 
-def push_stack(stack, item):
+def get_item(item):
     (buft, bufv) = item
 
     if buft == int:
@@ -89,32 +102,51 @@ def push_stack(stack, item):
     else:
         val = EId(buft, bufv)
 
-    stack.append(val)
+    return val
+
+
+def push_stack(stack, item):
+    stack.append(get_item(item))
 
 
 def run_stack(stack, scope):
-    constructs = {
-        'def': [EList, EBlock, EId],
-        'set': [Expr, EId],
-    }
+    constructs = [
+        ('def', [EList, EBlock, EId]),
+        ('set', [EId]),
+        ('set', [Expr, EId]),
+    ]
 
-    for op, p in constructs.items():
+    for op, p in constructs:
         if match_stack(stack, p):
             if op == 'set':
                 name = stack.pop()
-                value = stack.pop()
+
+                if stack:
+                    value = stack.pop()
+                else:
+                    value = None
 
                 if name.v in scope and scope[name.v]['is_callable']:
-                    yield from gen_call(name, value, scope)
+                    args = []
+                    if value:
+                        args.append(value)
+                    yield from emit_call(name, args, scope)
+                elif name.v == ',':
+                    scope['__'].append(value)
+                elif isinstance(value, EId) and value.v in scope and scope[value.v]['is_callable'] and scope['__']:
+                    call = list(emit_call(value, [], scope))[0].value
+                    yield from emit_set(name, call, scope)
                 else:
-                    yield from gen_set(name, value, scope)
+                    scope[name.v] = scope.get(value.v) or {'is_callable': True}
+                    value = resolve_value(value, scope)
+                    yield from emit_set(name, value, scope)
 
             elif op == 'def':
                 name = stack.pop()
                 block = stack.pop()
                 args = stack.pop()
 
-                yield from gen_def(name, args, block, scope)
+                yield from emit_def(name, args, block, scope)
 
     while True:
         try:
@@ -125,7 +157,11 @@ def run_stack(stack, scope):
 
 def match_stack(stack, parts):
     for i, item in enumerate(stack):
-        if isinstance(item, parts[i]):
+        try:
+            p = parts[i]
+        except IndexError:
+            break
+        if isinstance(item, p):
             return len(stack) == len(parts)
     return False
 
@@ -137,31 +173,45 @@ def resolve_value(value, scope):
         return ast.Num(n=value.v)
 
 
-def gen_expr(**kwargs):
+def expr(**kwargs):
     node = ast.Expr(**kwargs)
     node = ast.fix_missing_locations(node)
     return node
 
 
-def gen_call(name, value, scope):
-    yield gen_expr(value=ast.Call(func=ast.Name(id=name.v, ctx=ast.Load()),
-                   args=[resolve_value(value, scope)],
-                   keywords=[]))
+# id
+# expr id
+def emit_call(name, args, scope):
+    args2 = scope['__']
+    args2.extend(args)
+    args2 = [resolve_value(a, scope) for a in args2]
+
+    yield expr(value=ast.Call(func=ast.Name(id=name.v, ctx=ast.Load()),
+               args=args2,
+               kwargs=[],
+               starargs=[],
+               keywords=[]))
+
+    scope['__'] = []
 
 
-def gen_set(name, value, scope):
-    yield gen_expr(targets=[
+# id
+# expr id
+def emit_set(name, value, scope):
+    yield ast.fix_missing_locations(ast.Assign(targets=[
         ast.Name(id=name.v, ctx=ast.Store())
-    ], value=resolve_value(value, scope))
+    ], value=value))
 
 
-def gen_def(name, args, body, scope):
+# list body id
+def emit_def(name, args, body, scope):
     scope[name.v] = {'is_callable': True}
     args = [ast.arg(arg=a, annotation=None) for a in args.v]
+    args = ast.arguments(args=args, vararg=None, kwonlyargs=[], kw_defaults=[],
+                         kwarg=None, defaults=[])
     yield ast.fix_missing_locations(ast.FunctionDef(
         name=name.v,
-        args=ast.arguments(
-            args=args, kwonlyargs=[], kw_defaults=[], defaults=[]),
+        args=args,
         body=list(parse(body.v)),
         decorator_list=[]))
 
@@ -169,8 +219,20 @@ def gen_def(name, args, body, scope):
 def run(txt):
     body = list(parse(txt))
     wrapper = ast.Module(body=body)
+
+    print(astor.dump(wrapper))
+    print(astor.to_source(wrapper))
+
     co = compile(wrapper, '<ast>', 'exec')
     exec(co, {})
 
 
-run('(a,) [ a print ] foo. 2 foo')
+run('''
+
+(a,b) [ a print ] add.
+
+1,2, add result.
+
+result print.
+
+''')
